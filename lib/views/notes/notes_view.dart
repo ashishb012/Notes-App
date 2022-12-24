f4fb510a494873f4a621940e24d6bd777cbc4ef2
ashishb012 <ashishb0128@gmail.com>
2022-12-24 12:07:28 +0530

    step-4

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -122,6 +122,7 @@ class _NotesViewState extends State<NotesView> {
                   builder: (context, snapshot) {
                     switch (snapshot.connectionState) {
                       case ConnectionState.waiting:
+                      case ConnectionState.active:
                         return const Text("Waiting");
                       default:
                         return const CircularProgressIndicator();
