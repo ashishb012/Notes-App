4f11d166e663153834e02a47922d662accbb38a4
ashishb012 <ashishb0128@gmail.com>
2023-04-14 11:04:00 +0530

    step-26

--- lib/utilities/dailogs/error_dailogs.dart
+++ lib/utilities/dailogs/error_dailogs.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void showErrorDailog(
   BuildContext context,
   String text,
-) {
+) async {
   final snackBar = SnackBar(content: Text(text));
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  await HapticFeedback.vibrate();
 }
 // // Add await everywhere it is used
 // Future<void> showErrorDailog(
